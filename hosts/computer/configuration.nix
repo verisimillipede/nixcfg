3f18d9a6417b7f996358db94a519ffc40eaed72c
mike <mikehall@mikehall94.com>
2024-11-11 16:27:15 -0700

    typo

--- hosts/computer/configuration.nix
+++ hosts/computer/configuration.nix
@@ -2,7 +2,7 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, inputs, ouputs, lib, pkgs, ... }:
+{ config, inputs, outputs, lib, pkgs, ... }:
 
 {
   imports =
