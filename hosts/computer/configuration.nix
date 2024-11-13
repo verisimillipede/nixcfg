bc40c2c3b69fe303270fa6e7fe2e6c4776e10a3f
mike <mikehall@mikehall94.com>
2024-11-12 19:54:19 -0700

    cleaning up the mess

--- hosts/computer/configuration.nix
+++ hosts/computer/configuration.nix
@@ -31,40 +31,6 @@
   # Select internationalisation properties.
   i18n.defaultLocale = "en_CA.UTF-8";
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-
-  # Configure keymap in X11
-  services.xserver = {
-    layout = "us";
-    xkbVariant = "";
-  };
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.mike = {
@@ -80,11 +46,6 @@
     users.mike =
         import ../../home/mike/${config.networking.hostName}.nix;
   };
-# Install firefox.
-  programs.firefox.enable = true;
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
@@ -102,17 +63,70 @@
   #   enableSSHSupport = true;
   # };
 
+  # Pulseaudio
+  hardware.pulseaudio.enable = false;
+
+  # Services
+  services = {
+
+    # Keyd
+    keyd = {
+      enable = true;
+
+      # Swap capslock with ctrl + esc
+      keyboards.default.settings = {
+        main = {
+          capslock = "overload(control, esc)";
+          esc = "capslock";
+        };
+      };
+    };
+
+    # Tailscale
+    tailscale.enable = true;
+
+    # Printing
+    printing = {
+      enable = true;
+      drivers = [ pkgs.brlaser ];
+    };
+
+    # Avahi
+    avahi = {
+      enable = true;
+      nssmdns4 = true;
+      openFirewall = true;
+    };
+
+
+    # Pipewire
+    pipewire = {
+      enable = true;
+      alsa = {
+        enable = true;
+        support32Bit = true;
+      };
+    };
+
+    # Xserver
+    xserver = {
+      enable = true;
+      displayManager.gdm.enable = true;
+      desktopManager.gnome.enable = true;
+
+      xkb = {
+        layout = "us";
+        variant = "";
+      };
+    };
+
+    # desktopManager.cosmic.enable = false;
+    # displayManager.cosmic-greeter.enable = false;
+  };
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-  services.tailscale.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+  # services.openssh.enable = true;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
