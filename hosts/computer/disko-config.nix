e83e3ce5505e3e857980e218302e520196149400
mike <mikehall@mikehall94.com>
2024-11-17 21:49:37 -0700

    pray for me

--- hosts/computer/disko-config.nix
+++ hosts/computer/disko-config.nix
@@ -1,14 +1,14 @@
 {
   disko.devices = {
     disk = {
-      x = {
+      nixos = {
         type = "disk";
-        device = "/dev/sdx";
+        device = "/dev/nvme0n1";
         content = {
           type = "gpt";
           partitions = {
             ESP = {
-              size = "64M";
+              size = "512M";
               type = "EF00";
               content = {
                 type = "filesystem";
@@ -27,76 +27,19 @@
           };
         };
       };
-      y = {
-        type = "disk";
-        device = "/dev/sdy";
-        content = {
-          type = "gpt";
-          partitions = {
-            zfs = {
-              size = "100%";
-              content = {
-                type = "zfs";
-                pool = "zroot";
-              };
-            };
-          };
-        };
-      };
     };
     zpool = {
       zroot = {
         type = "zpool";
-        mode = "mirror";
-        # Workaround: cannot import 'zroot': I/O error in disko tests
-        options.cachefile = "none";
-        rootFsOptions = {
-          compression = "zstd";
-          "com.sun:auto-snapshot" = "false";
-        };
         mountpoint = "/";
-        postCreateHook = "zfs list -t snapshot -H -o name | grep -E '^zroot@blank$' || zfs snapshot zroot@blank";
-
         datasets = {
-          zfs_fs = {
+          root = {
             type = "zfs_fs";
-            mountpoint = "/zfs_fs";
-            options."com.sun:auto-snapshot" = "true";
+            mountpoint = "/";
           };
-          zfs_unmounted_fs = {
+          home = {
             type = "zfs_fs";
-            options.mountpoint = "none";
-          };
-          zfs_legacy_fs = {
-            type = "zfs_fs";
-            options.mountpoint = "legacy";
-            mountpoint = "/zfs_legacy_fs";
-          };
-          zfs_testvolume = {
-            type = "zfs_volume";
-            size = "10M";
-            content = {
-              type = "filesystem";
-              format = "ext4";
-              mountpoint = "/ext4onzfs";
-            };
-          };
-          encrypted = {
-            type = "zfs_fs";
-            options = {
-              mountpoint = "none";
-              encryption = "aes-256-gcm";
-              keyformat = "passphrase";
-              keylocation = "file:///tmp/secret.key";
-            };
-            # use this to read the key during boot
-            # postCreateHook = ''
-            #   zfs set keylocation="prompt" "zroot/$name";
-            # '';
-          };
-          "encrypted/test" = {
-            type = "zfs_fs";
-            mountpoint = "/zfs_crypted";
+            mountpoint = "/home";
           };
         };
       };
