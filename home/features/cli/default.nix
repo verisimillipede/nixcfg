e234924dfcc9ad57fc758d8211860711043a5b10
mike <mikehall@mikehall94.com>
2024-11-23 19:18:14 -0700

    modularizing my user configs

---
+++ home/features/cli/default.nix
@@ -0,0 +1,49 @@
+{pkgs, ...}: {
+  programs = {
+    zoxide = {
+      enable = true;
+      enablefishintegration = true;
+    };
+    eza = {
+      enable = true;
+      enablefishintegration = true;
+      enablebashintegration = true;
+      extraOptions = ["-l" "--icons" "-a" "--git"];
+    };
+    bat = {
+      enable = true;
+    };
+  };
+
+  home.packages = with pkgs; [
+    alejandra
+    bc
+    comma
+    coreutils
+    devenv
+    direnv
+    fabric-ai
+    fd
+    gcc
+    go
+    htop
+    httpie
+    jq
+    just
+    lf
+    lazygit
+    nix-index
+    procs
+    progress
+    ripgrep
+    tldr
+    trash-cli
+    unimatrix
+    unzip
+    wttrbar
+    wireguard-tools
+    yazi
+    zellij-ps
+    zip
+  ];
+}
