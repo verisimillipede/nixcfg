a4ffa1a844f15d357ea457d3bbfa90dc49953987
mike <mikehall@mikehall94.com>
2024-11-25 21:03:41 -0700

    GOT IT WORKING. The issue must have been setting up paths, which is fine in the user space, but at login it confuses the system.

--- home/features/cli/default.nix
+++ home/features/cli/default.nix
@@ -3,27 +3,27 @@
     ./fish.nix
   ];
 
-  # programs = {
-  #   zoxide = {
-  #     enable = true;
-  #     enableFishIntegration = true;
-  #   };
-  #   eza = {
-  #     enable = true;
-  #     enableFishIntegration = true;
-  #   };
-  #   bat = {
-  #     enable = true;
-  #   };
-  #   atuin = {
-  #     enable = true;
-  #     enableFishIntegration = true;
-  #   };
-  #   starship = {
-  #     enable = true;
-  #     enableFishIntegration = true;
-  #   };
-  # };
+  programs = {
+    zoxide = {
+      enable = true;
+      enableFishIntegration = true;
+    };
+    eza = {
+      enable = true;
+      enableFishIntegration = true;
+    };
+    bat = {
+      enable = true;
+    };
+    atuin = {
+      enable = true;
+      enableFishIntegration = true;
+    };
+    starship = {
+      enable = true;
+      enableFishIntegration = true;
+    };
+  };
 
   home.packages = with pkgs; [
     alejandra
