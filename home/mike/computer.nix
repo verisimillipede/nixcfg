8deac1c853b6081dc57ad91faeebe7544e1f90fe
mike <mikehall@mikehall94.com>
2024-11-30 19:24:44 -0700

    this should work to get home-manager functioning on my computer

--- home/mike/computer.nix
+++ home/mike/computer.nix
@@ -1,1 +1,12 @@
-{config, ...}: {imports = [./home.nix ../common];}
+{...}: {
+  imports = [
+    ./home.nix
+    ../features/cli
+    ../common
+  ];
+
+  features.cli = {
+    fish.enable = true;
+    tmux.enable = true;
+  };
+}
--- hosts/computer/configuration.nix
+++ hosts/computer/configuration.nix
@@ -36,6 +36,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    vim
   ];
 
   # Pulseaudio
