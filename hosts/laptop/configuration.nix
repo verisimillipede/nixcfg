fb3263bd75df5d9f747a627c2112c5542f1f9044
mike <mikehall@mikehall94.com>
2025-02-23 13:51:17 -0700

    message

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -12,7 +12,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.supportedFilesystems = ["zfs"];
-  boot.kernelPackages = pkgs.linuxKernel.packages.linux_zen.zfs_unstable;
+  boot.kernelPackages = pkgs.linuxKernel.packages.linux_zen;
   boot.zfs = {
     package = pkgs.zfs_unstable;
   };
