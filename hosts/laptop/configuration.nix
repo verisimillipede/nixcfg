1413edcd5943d71be0058ba6292912058c08e649
mike <mikehall@mikehall94.com>
2025-05-04 07:51:56 -0500

    timzezone(back to the real world): chestermere time...

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -21,8 +21,7 @@
   networking.hostName = "laptop";
 
   # Set your time zone.
-  # time.timeZone = "America/Edmonton";
-  time.timeZone = "America/Cancun";
+  time.timeZone = "America/Edmonton";
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_CA.UTF-8";
