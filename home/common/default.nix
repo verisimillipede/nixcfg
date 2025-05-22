ae52cc2c8e06cfb6db0b1af0537f825f7b48538c
mike <mikehall@mikehall94.com>
2025-05-21 22:00:30 -0600

    .

--- home/common/default.nix
+++ home/common/default.nix
@@ -37,10 +37,27 @@
     TERMINAL = "kitty";
   };
 
+  xdg.desktopEntries.zen = {
+    name = "Zen Browser";
+    genericName = "Web Browser";
+    exec = "zen %U";
+    terminal = false;
+    categories = ["Network" "WebBrowser"];
+    mimeType = [
+      "text/html"
+      "text/xml"
+      "application/xhtml+xml"
+      "x-scheme-handler/http"
+      "x-scheme-handler/https"
+    ];
+  };
+
   xdg.mimeApps = {
     enable = true;
     defaultApplications = {
       "text/html" = ["zen.desktop"];
+      "text/xml" = ["zen.desktop"];
+      "application/xhtml+xml" = ["zen.desktop"];
       "x-scheme-handler/http" = ["zen.desktop"];
       "x-scheme-handler/https" = ["zen.desktop"];
     };
