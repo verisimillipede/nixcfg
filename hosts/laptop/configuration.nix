a4503fff9328c64f6fc89b82ecc88b71f070de96
mike <mikehall@mikehall94.com>
2025-07-05 14:59:46 -0600

    change

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -49,6 +49,8 @@
       authentication = pkgs.lib.mkOverride 10 ''
         #type database  DBuser  auth-method
         local all       all     trust
+        host    all       all   127.0.0.1/32   trust
+        host    all       all   ::1/128        trust
       '';
     };
   };
